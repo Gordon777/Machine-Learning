360bef63111385b0d063f63f4b5c0e7a19b00169
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-26 07:14:55 +0000

    Adding tarfile member sanitization to extractall()

--- ML HW2/pre-process.py
+++ ML HW2/pre-process.py
@@ -156,15 +156,72 @@ if __name__ == '__main__':
     print('Extracting cars_train.tgz...')
     if not os.path.exists(r'./cars_train'):
         with tarfile.open(r'./cars_train.tgz', "r:gz") as tar:
-            tar.extractall()
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar)
     print('Extracting cars_test.tgz...')
     if not os.path.exists(r'./cars_test'):
         with tarfile.open(r'./cars_test.tgz', "r:gz") as tar:
-            tar.extractall()
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar)
     print('Extracting car_devkit.tgz...')
     if not os.path.exists(r'./devkit'):
         with tarfile.open(r'./car_devkit.tgz', "r:gz") as tar:
-            tar.extractall()
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar)
 
     cars_meta = scipy.io.loadmat(r'./devkit/cars_meta')
     class_names = cars_meta['class_names']  # shape=(1, 196)
